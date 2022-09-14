fed61bba099d9885be50a0b98bf7db8ab37906c6
Ruriê <rurie.miguel@gmail.com>
2022-09-14 13:24:32 -0300

    BdeBenção

--- src/main/java/com/generation/solution/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/solution/security/BasicSecurityConfig.java
@@ -1,47 +1,51 @@
 package com.generation.solution.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class BasicSecurityConfig {
-	
-		@Bean 
-	    public PasswordEncoder passwordEncoder() {
-	        return new BCryptPasswordEncoder();
-	    }
 
-	    @Bean 
-	    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-	        return authenticationConfiguration.getAuthenticationManager();
-	    }
-	    
-	    @Bean
-	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	        http
-	            .sessionManagement() 
-	            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	            .and().csrf().disable() 
-	            .cors(); 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-	        http
-	            .authorizeHttpRequests((auth) -> auth
-	                .antMatchers("/usuarios/logar").permitAll() 
-	                .antMatchers("/usuarios/cadastrar").permitAll() 
-	                .antMatchers(HttpMethod.OPTIONS).permitAll() 
-	                .anyRequest().authenticated())
-	            .httpBasic();
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().csrf().disable()
+            .cors();
 
-	        return http.build(); 
-	    }
+        http
+            .authorizeHttpRequests((auth) -> auth
+                .antMatchers("/usuarios/logar").permitAll()
+                .antMatchers("/usuarios/cadastrar").permitAll()
+//                .antMatchers("/usuarios/atualizar").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated())
+            .httpBasic();
 
-	}
+        return http.build();
 
+    }
 
+}
