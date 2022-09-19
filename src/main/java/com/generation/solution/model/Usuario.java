31dd1dd9ffc84b248c7194647c6fd56fa704b02e
Pddro1 <pedrohcamargon@gmail.com>
2022-09-19 09:51:21 -0300

    deploy

--- src/main/java/com/generation/solution/model/Usuario.java
+++ src/main/java/com/generation/solution/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -26,7 +28,8 @@ public class Usuario {
 
 	@NotNull(message = "O Atributo Nome é Obrigatório!")
 	private String nome;
-
+	
+	@Schema(example = "email@email.com")
 	@NotNull(message = "O Atributo Usuário é Obrigatório!")
 	@Email(message = "O Atributo Usuário deve ser um email válido!")
 	private String usuario;
